d886f14a0837c6e2509f0b67919a0d8e3eef6a81
LBKinson <laura.kinson@gmail.com>
2015-04-15 14:50:31 -0700

    added sam user & password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+ 	before_filter :authenticate
+
+	protected
+
+	def authenticate
+	  authenticate_or_request_with_http_basic do |username, password|
+	    username == "sam" && password == "foobar"
+	  end
+	end
+
   protect_from_forgery with: :exception
 end
